fe78b1ee044dea007b0a8bacb88ef4d5d5b7367c
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-14 14:42:40 -0700

    got subtitle state to render on page

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor({ title, setTitle }) {
+export default function Editor({ title, setTitle, subTitle, setSubTitle }) {
   return (
     <div className="editor">
       <div className="form-control">
@@ -10,7 +10,7 @@ export default function Editor({ title, setTitle }) {
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
-        <input type="text" />
+        <input type="text" name="subtitle" value={subTitle} onChange={(e) => setSubTitle(e.target.value)} />
         <label>Subtitle</label>
       </div>
       <div className="form-control">
